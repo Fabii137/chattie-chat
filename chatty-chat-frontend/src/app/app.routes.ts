db4c162de6d7e10ac22b2e46c9663624dd780346
Fabii137 <krsticfabian@gmail.com>
2025-06-07 00:35:42 +0200

    removed template code, added basic structure

--- chatty-chat-frontend/src/app/app.routes.ts
+++ chatty-chat-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent, title: 'Home'},
+    { path: 'login', component: LoginComponent, title: 'Login'},
+    { path: '**', redirectTo: 'home'}
+];
