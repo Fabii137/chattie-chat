fbc8e646e1c4688ff0fd55b625811ad50b1bffb7
Fabii137 <krsticfabian@gmail.com>
2025-06-08 22:07:02 +0200

    added basic room page

--- chattie-chat-frontend/src/app/components/room/room.component.ts
+++ chattie-chat-frontend/src/app/components/room/room.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Room } from '../../../entities/room.entity';
+import { RoomService } from '../../../services/http-backend/room.service';
+import { SocketService } from '../../../services/socket.service';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../entities/user.entity';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-room',
@@ -6,6 +13,31 @@ import { Component } from '@angular/core';
   templateUrl: './room.component.html',
   styleUrl: './room.component.css'
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
+  room: Room | null = null;
+  roomId: number | null = null;
+
+  constructor(private roomService: RoomService, private authService: AuthService, private socketService: SocketService, private route: ActivatedRoute) {
+    
+  }
+
+  async ngOnInit(): Promise<void> {
+      await this.loadRoom();
+  }
+
+  async loadRoom() {
+    const id = this.route.snapshot.paramMap.get("roomId");
+    if (!id)
+      return null;
+
+    const user = this.authService.currentUser;
+    if(!user)
+      return null;
+
+    this.roomId = Number(id);
+
+    const room = await firstValueFrom(this.roomService.getRoomById(this.roomId, user.id));
+    return room;
+  }
 
 }
