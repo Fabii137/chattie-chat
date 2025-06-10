898850a72cb4cc69ceb0a630516d38102ae226ad
Fabian KRSTIC <Fabian.KRSTIC@bachmann.info>
2025-06-10 11:54:23 +0200

    refactor

--- chattie-chat-frontend/src/services/socket.service.ts
+++ chattie-chat-frontend/src/services/socket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Message } from '../entities/message.entity';
 
 @Injectable({ providedIn: 'root' })
