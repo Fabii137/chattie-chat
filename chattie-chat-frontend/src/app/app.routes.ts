82bc086b87e65ee1a15769ae8a6374a35898eb85
KRSTIC.Fabian <FAKR@bachmann.at>
2025-06-12 13:37:37 +0200

    added server, refactor

--- chattie-chat-frontend/src/app/app.routes.ts
+++ chattie-chat-frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { FriendsComponent } from './components/friends/friends.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from '../guards/auth.guard';
 import { RoomComponent } from './components/room/room.component';
+import { ServerComponent } from './components/server/server.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'friends', pathMatch: 'full'},
-    { path: 'friends', component: FriendsComponent, title: 'Friends', canActivate: [AuthGuard]},
-    { path: 'login', component: LoginComponent, title: 'Login'},
-    { path: 'rooms', component: RoomComponent, title: 'Rooms', canActivate: [AuthGuard]},
-    { path: 'rooms/:roomId', component: RoomComponent, title: 'Room', canActivate: [AuthGuard]},
-    { path: '**', redirectTo: 'friends'}
+    { path: '', redirectTo: 'friends', pathMatch: 'full' },
+    { path: 'friends', component: FriendsComponent, title: 'Friends', canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent, title: 'Login' },
+    { path: 'rooms', component: RoomComponent, title: 'Rooms', canActivate: [AuthGuard] },
+    { path: 'rooms/:roomId', component: RoomComponent, title: 'Room', canActivate: [AuthGuard] },
+    { path: 'servers/:serverId', component: ServerComponent, title: 'Server', canActivate: [AuthGuard] },
+    { path: '**', redirectTo: 'friends' }
 ];
