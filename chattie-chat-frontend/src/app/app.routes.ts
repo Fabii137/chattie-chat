25475ddda0409825a694eca5a3eedaf696a21243
Fabian KRSTIC <Fabian.KRSTIC@bachmann.info>
2025-07-01 09:11:03 +0200

    switched routes to loadComponent

--- chattie-chat-frontend/src/app/app.routes.ts
+++ chattie-chat-frontend/src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
-import { FriendsComponent } from './components/friends/friends.component';
-import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from '../guards/auth.guard';
-import { RoomComponent } from './components/room/room.component';
-import { ServerComponent } from './components/server/server.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'friends', pathMatch: 'full' },
-    { path: 'friends', component: FriendsComponent, title: 'Friends', canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent, title: 'Login' },
-    { path: 'rooms', component: RoomComponent, title: 'Rooms', canActivate: [AuthGuard] },
-    { path: 'rooms/:roomId', component: RoomComponent, title: 'Room', canActivate: [AuthGuard] },
-    { path: 'servers/:serverId', component: ServerComponent, title: 'Server', canActivate: [AuthGuard] },
+    { path: 'friends', loadComponent: () => import('./components/friends/friends.component').then(m => m.FriendsComponent), title: 'Friends', canActivate: [AuthGuard] },
+    { path: 'login', loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+    { path: 'rooms', loadComponent: () => import('./components/room/room.component').then(m => m.RoomComponent), title: 'Rooms', canActivate: [AuthGuard] },
+    { path: 'rooms/:roomId', loadComponent: () => import('./components/room/room.component').then(m => m.RoomComponent), title: 'Room', canActivate: [AuthGuard] },
+    { path: 'servers/:serverId', loadComponent: () => import('./components/server/server.component').then(m => m.ServerComponent), title: 'Server', canActivate: [AuthGuard] },
     { path: '**', redirectTo: 'friends' }
 ];
